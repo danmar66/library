7b28f8fdfc028a88bf74e743b96295a8d3046ec4
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-02-26 23:11:04 +0200

    Remove alpha-numeric validation

--- validators/book.validator.js
+++ validators/book.validator.js
@@ -4,8 +4,8 @@ const validator = (schema) => (payload) =>
   schema.validate(payload, { abortEarly: false })
 
 const bookSchema = Joi.object({
-  title: Joi.string().alphanum().min(3).max(30).lowercase().required(),
-  author: Joi.string().alphanum().min(3).max(30).lowercase().required(),
+  title: Joi.string().min(3).max(30).lowercase().required(),
+  author: Joi.string().min(3).max(30).lowercase().required(),
 })
 
 exports.validateBook = validator(bookSchema)

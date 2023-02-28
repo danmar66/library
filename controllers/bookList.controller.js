b8ea8ad6f6741791b9d071475d74810080a4fdc2
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-02-28 15:26:55 +0200

    Convert class to arrow functions

--- controllers/bookList.controller.js
+++ controllers/bookList.controller.js
@@ -1,77 +1,45 @@
 const Book = require('../models/book.model')
 const { validateBook } = require('../validators/book.validator')
 
-class bookController {
-  async create(req, res) {
-    try {
-      const { error, value } = validateBook(req.body)
-      if (error) {
-        return res.send(error.details)
-      }
-      const book = await Book.create({
-        ...value,
-      })
-      return res.json({ book })
-    } catch (error) {
-      console.error(error)
-      res.status(424).json({ error: 'Unknown error' })
-    }
-  }
-
-  async getAll(req, res) {
-    try {
-      let { ...options } = req.query
-      const books = await Book.paginate({}, options ? options : null)
-      return res.json({ books })
-    } catch (error) {
-      console.error(error.message)
-      res.status(424).json({ error: 'Unknown error' })
-    }
-  }
-
-  async getOne(req, res) {
-    try {
-      const { id } = req.params
-      const book = await Book.findById({ _id: id })
-      return res.json(book)
-    } catch (error) {
-      console.error(error.message)
-      res.status(424).json({ error: 'Unknown error' })
-    }
-  }
-
-  async update(req, res) {
-    try {
-      const { id } = req.params
-      const { error, value } = validateBook(req.body)
-      if (error) {
-        return res.send(error.details)
-      }
-      const newBook = await Book.findByIdAndUpdate(
-        { _id: id },
-        { ...value },
-        { new: true }
-      )
-      return res.json(newBook)
-    } catch (error) {
-      console.error(error.message)
-      res.status(424).json({ error: 'Unknown error' })
-    }
-  }
-
-  async delete(req, res) {
-    try {
-      const { id } = req.params
-      const deleted = await Book.findByIdAndDelete({ _id: id })
-      if (!deleted) {
-        return res.json({ message: 'Already deleted' })
-      }
-      return res.json({ message: 'Book deleted', deleted })
-    } catch (error) {
-      console.error(error.message)
-      res.status(424).json({ error: 'Unknown error' })
-    }
-  }
+const create = async (req, res) => {
+  const { error, value } = validateBook(req.body)
+  if (error) throw error
+  const book = await Book.create({
+    ...value,
+  })
+  return res.status(200).json({ book })
 }
 
-module.exports = new bookController()
+const getAll = async (req, res) => {
+  let { ...options } = req.query
+  const books = await Book.paginate({}, options ? options : null)
+  return res.status(200).json({ books })
+}
+
+const getOne = async (req, res) => {
+  const { id } = req.params
+  const book = await Book.findById({ _id: id })
+  if (!book) throw new Error("Book doesn't exist")
+  return res.status(200).json(book)
+}
+
+const update = async (req, res) => {
+  const { id } = req.params
+  const { error, value } = validateBook(req.body)
+  if (error) throw error
+  const newBook = await Book.findByIdAndUpdate(
+    { _id: id },
+    { ...value },
+    { new: true }
+  )
+  return res.status(200).json(newBook)
+}
+
+const remove = async (req, res) => {
+  const { id } = req.params
+  const deleted = await Book.findByIdAndDelete({ _id: id })
+  if (!deleted) throw new Error("Book doesn't exist")
+  return res.status(200).json({ message: 'Book deleted', deleted })
+}
+
+module.exports = { create, getAll, getOne, update, remove }

d3628b8df0fdc94004d4e7f5bcb9c799efff6125
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-02-28 15:24:01 +0200

    Add errorHandler middleware

---
+++ middlewares/errorHandler.middleware.js
@@ -0,0 +1,14 @@
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+
+  if (error.name === 'ValidationError') {
+    return res.status(400).send({
+      type: 'ValidationError',
+      details: error.details,
+    })
+  }
+
+  return res.status(400).send(error.message)
+}
+
+module.exports = errorHandler
